00747f0648cf154e9c0349dc894a4664fe095063
zbretz <zachary.bretz@gmail.com>
2025-10-03 14:29:33 -0600

    Added 'test account' functionality for tnc review

--- routes.js
+++ routes.js
@@ -18,6 +18,34 @@ router.get('/policies', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'policies.html'));
 });
 
+router.get('/booking', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'booking.html'));
+});
+
+router.get('/account', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'account.html'));
+});
+
+
+/* ****** */
+
+// router.get('/tpca', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'tpca.html'));
+// });
+
+// router.get('/ski', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'ski.html'));
+// });
+
+// router.get('/driver', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'driver.html'));
+// });
+
+// router.get('/profile', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'profile.html'));
+// });
+
+
 
 
 module.exports = router;
